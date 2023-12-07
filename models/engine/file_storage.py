0c71a1386bd682ff23f1b13d59b3684bdb13f278
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2023-12-07 02:36:18 -0800

    task 5 done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,15 +15,11 @@ class FileStorage:
         self.__objects[key] = obj
 
     def save(self):
-        """
+        
         for key, obj in self.__objects.items():
             self.__objects[key] = obj.to_dict()
             with open(self.__file_path, 'w') as file:
                 json.dump(self.__objects, file)
-
-        """
-        with open(self.__file_path, "w") as file:
-            json.dump(self.__objects, file)
 
     def reload(self):
         """
